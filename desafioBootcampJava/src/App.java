6d0a2f4ae0623918f488ab5bff14d6e6dfabc0ea
olimaandreza <94322254+olimaandreza@users.noreply.github.com>
2022-08-09 07:15:55 -0300

    Add Mentoria e Curso

--- desafioBootcampJava/src/App.java
+++ desafioBootcampJava/src/App.java
@@ -1,5 +1,28 @@
+import java.time.LocalDate;
+
+import desafio.dominio.Curso;
+import desafio.dominio.Mentoria;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso java");
+        curso1.setDescricao("Curso para iniciantes");
+        curso1.setCargaHoraria(62);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso java 2");
+        curso2.setDescricao("Curso intermediário");
+        curso2.setCargaHoraria(42);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Mentoria java");
+        mentoria1.setDescricao("mentoria java");
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria1);
+
     }
 }
---
+++ desafioBootcampJava/src/desafio/dominio/Curso.java
@@ -0,0 +1,47 @@
+package desafio.dominio;
+
+public class Curso {
+    /* Atributos */ 
+    private String titulo;
+    private String descricao;
+    private int cargaHoraria;
+    
+    /* Construtor */
+    public Curso() {
+    }
+
+    /* Getters and Setters */
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    /* toString */
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return "Curso{"+
+                "titulo='" + titulo + "\'" +
+                ", descricao='" + descricao + "\'" +
+                ", cargaHoraria=" + cargaHoraria + '}';
+
+    }
+    
+
+    
+    
+}
